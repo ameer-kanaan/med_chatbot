26588ce303c257c652786a7bd9d2bd0631f3ebe5
ameer-kanaan <41696743+ameer-kanaan@users.noreply.github.com>
2025-12-24 02:00:01 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical_chabot",
+    version = "0.1.0",
+    author = "Emir Kenanoglu",
+    author_email = "ameer.kannan@yahoo.com",
+    packages = find_packages(),
+    install_requires = []
+)
